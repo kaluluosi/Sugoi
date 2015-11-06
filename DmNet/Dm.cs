47731f58c4ee895227c45e766236bfbf5d1c6b02
kaluluosi111 <kaluluosi@gmail.com>
2015-11-06 09:54:42 +0800

    Revert "Revert "解决了大漠组件自动注册问题""
    This reverts commit 9640df14e6f9b14c2e8e383e2684d9425337087a.

--- DmNet/Dm.cs
+++ DmNet/Dm.cs
@@ -3,17 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using dmNet;
+using Dm;
 using System.Diagnostics;
 using System.IO;
+using System.Runtime.InteropServices;
 
-namespace DmNet
-{
+namespace DmNet {
     /// <summary>
     /// 大漠对象单例工厂
     /// </summary>
-    public class Dm
-    {
+    public class Dm {
+        private const string dllDefaultPath = "./dm.dll";
+
         private static dmsoft dm;
 
         public static bool IsRegisted {
@@ -28,25 +29,36 @@ namespace DmNet
         /// </summary>
         public static dmsoft Default {
             get {
+                //auto regist
+                try {
+                    if (IsRegisted == false)
+                        RegistDM();
+                }
+                catch (FileNotFoundException ex) {
+                    throw ex;
+                }
                 return dm == null ? dm = new dmsoft() : dm;
             }
         }
 
 
-        public static void RegistDM(string path = "./dm.dll") {
+        public static void RegistDM(string path = dllDefaultPath) {
+            if (File.Exists(dllDefaultPath)==false) {
+                throw new FileNotFoundException(dllDefaultPath + " is not found. Make sure the dll is in the root path.");
+            }
             string strCmd = string.Format("regsvr32 {0}", path);
             try {
                 Process myProcess = new Process();
                 ProcessStartInfo myProcessStartInfo = new ProcessStartInfo("cmd.exe");
-                myProcessStartInfo.Verb = "RunAs";
+                myProcessStartInfo.Verb = "runas";
                 myProcessStartInfo.UseShellExecute = false;
                 myProcessStartInfo.CreateNoWindow = true;
                 myProcessStartInfo.RedirectStandardOutput = true;
                 myProcess.StartInfo = myProcessStartInfo;
-                myProcessStartInfo.Arguments = "/c " + strCmd;
+                myProcessStartInfo.Arguments = "/s " + strCmd;
                 myProcess.Start();
             }
-            catch(System.Exception ex) {
+            catch (System.Exception ex) {
                 throw ex;
             }
         }
---
+++ TestApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DmNet;
+using DmNet.Window;
+
+namespace TestApp {
+    class Program {
+        static void Main(string[] args) {
+            DmNet.Dm.RegistDM();
+        }
+    }
+}
